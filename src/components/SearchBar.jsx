ea8512e9311163f9aa8b0a0abbb15c4795af0afb
Gazzito <telmo9@hotmail.com>
2023-09-05 21:09:32 +0100

    SearchField created

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,10 @@
+import { Input } from "@material-tailwind/react"
+
+
 const SearchBar = () => {
     return <>
     <div className="pt-10 px-10">
-    <div className="bg-secondary rounded-lg p-5">Hello</div>
+    <div className="bg-secondary rounded-lg p-5"><Input label="Search a Pokemon" /></div>
     </div>
     </>
 
